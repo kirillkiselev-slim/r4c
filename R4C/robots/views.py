e16cc2231d5a1ade9938fd3304f994516d2d436a
kirill_kiselev99 <kisabot99@gmail.com>
2023-10-04 23:57:47 +0300

    add function create_robot

--- R4C/robots/views.py
+++ R4C/robots/views.py
@@ -1,4 +1,31 @@
-from django.shortcuts import render
+import json
+from .models import Robot
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from .serializers import RobotSerializer
 
+@csrf_exempt
+def create_robot(request):
+    if request.method == "POST":
+        dt = json.loads(request.body)
+        serial = dt.get('serial')
+        model = dt.get('model')
+        version = dt.get('version')
+        created = dt.get('created')
 
-# Create your views here.
+        serializer = RobotSerializer(data=dt)
+
+        if serializer.is_valid():
+
+            if serial and model and version and created:
+                robot = Robot(serial=serial.upper(), model=model.upper(),
+                              version=version.upper(), created=created)
+                robot.save()
+                return JsonResponse({"success": True})
+            else:
+                return JsonResponse({"success": False, "error": "Invalid data"})
+        else:
+            return JsonResponse({"success": False, "error": serializer.errors})
+    else:
+        return JsonResponse({"success": False, "error": "Invalid request method"})
+
