0c3d796443ffac03ffc5b00a192d79be3960460a
kirill_kiselev99 <kisabot99@gmail.com>
2023-10-06 14:13:34 +0300

    add generate_excel_report and download_excel_report functions

--- R4C/robots/views.py
+++ R4C/robots/views.py
@@ -1,8 +1,13 @@
 import json
 from .models import Robot
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 from .serializers import RobotSerializer
+import openpyxl
+from django.db.models import Count
+from datetime import timedelta
+from django.utils import timezone
+
 
 @csrf_exempt
 def create_robot(request):
@@ -28,4 +33,44 @@ def create_robot(request):
             return JsonResponse({"success": False, "error": serializer.errors})
     else:
         return JsonResponse({"success": False, "error": "Invalid request method"})
+
+
+def generate_excel_report():
+    filepath = "../robots_reports.xlsx"
+    last_week = timezone.now() - timedelta(days=7)
+
+    wb = openpyxl.Workbook()
+    default_sheet = wb.active
+    excel_column_names = ['Модель', 'Версия', 'Количество за неделю']
+
+    for col_num, col_name in enumerate(excel_column_names, start=1):
+        default_sheet.cell(row=1,column=col_num).value = col_name
+
+    data = Robot.objects.filter(created__gte=last_week).values_list('model', 'version').annotate(count=Count('version'))
+
+    models = set(row[0] for row in data)
+    for model in models:
+        new_sheet = wb.create_sheet(title=model)
+        new_sheet.append(excel_column_names)
+
+        for row_data in data:
+            if row_data[0] == model:
+                new_sheet.append(row_data)
+
+    wb.remove(wb.active)
+
+    wb.save(filepath)
+
+    return filepath
+
+report = generate_excel_report()
+
+
+@csrf_exempt
+def download_excel_report(request):
+    file_path = "../robots_reports.xlsx"
+    with open(file_path, 'rb') as file:
+        response = HttpResponse(file.read(), content_type='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
+        response['Content-Disposition'] = 'attachment; filename=robots_reports.xlsx'
+    return response
 
