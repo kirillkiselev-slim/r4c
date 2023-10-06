424fc1744d248be38b65b39d28aec59a80e1a164
kirill_kiselev99 <kisabot99@gmail.com>
2023-10-06 14:15:11 +0300

    add paths to create a robot in db and another path to generate excel report

--- R4C/robots/urls.py
+++ R4C/robots/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import create_robot
+from .views import create_robot, download_excel_report
 
 
 APP_NAME = "robots"
 urlpatterns = [
-    path("robots/", create_robot, name="create-robot"),
+    path("robots", create_robot, name="create-robot"),
+    path("robots/excel-report", download_excel_report, name="download-robot-report")
 ]
